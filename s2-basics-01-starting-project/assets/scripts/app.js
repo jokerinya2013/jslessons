1e20f7460872b83e573857e339f19fd2f7e2d8d6
Ibrahim Sakaci <ibrahim@unico.aero>
2020-02-20 23:53:46 +0300

    functions started, using () and not using(), order, local-global declaration

--- s2-basics-01-starting-project/assets/scripts/app.js
+++ s2-basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,26 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
+}
+
+// currentResult = add(1, 2);
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+addBtn.addEventListener('click', add);
+add;
+//
+// Notlar...
+//
 // altgr + , = backtick yapar
 // altgr + ? = backslaht yapar
 // css de white-space: pre; metni ve boşlukları olduğu gibi gösterir
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-// let errorMessage = 'An error \n' + 'occured!';
-
-outputResult(currentResult, calculationDescription);
+// let errorMessage = 'An error \n' + 'occured!'; line break yapar
+// functions name(){} şeklinde tanımlananlar tanımlanmadan önce kullanılabiir
+// return sonrası kodlar iptal edilir
+// addEventListener() iki parameter alır, ilki str ikincisi fonks ama sadece adı parentez yok
+// parentez koymadaki mantık, js yukarıdan aşağı kodu okurken git ve o fonk çalıştır demek
+// ama bazı durumlarda bunu yapmasını istemeyiz. bu yüzden sadece adını yazarız,
+// bir tetikleyici olduğunda uygula deriz.
