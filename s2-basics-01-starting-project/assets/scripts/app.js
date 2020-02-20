31f7a1141b199104cd3821379ba236e0dcade0b8
ibrahim <ibrahim@unico.aero>
2020-02-20 17:27:00 +0300

    backtick, backslash and strings observed

--- s2-basics-01-starting-project/assets/scripts/app.js
+++ s2-basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,10 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
-
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+// altgr + , = backtick yapar
+// altgr + ? = backslaht yapar
+// css de white-space: pre; metni ve boşlukları olduğu gibi gösterir
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+// let errorMessage = 'An error \n' + 'occured!';
 
 outputResult(currentResult, calculationDescription);
