78dd3922425988c517be5042859b10c4ed7b7ca1
unknown <ibrahim@unico.aero>
2020-03-05 17:21:46 +0300

    func declaration/expression

--- s6-functions-01-starting-code/app.js
+++ s6-functions-01-starting-code/app.js
@@ -1,1 +1,57 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = 'ROCK';
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener('click', function() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
+
+// Notlar
+// object içindeki func lara method denir
+// argument function deneme('Max') --> Max ==> argument
+//  parameter function deneme(name) --> name ==> parametre
+// function lar objectlerdir
+// function diye başlayarak belirtince declaration olur
+// declarations: her yerde tanımlanıp kullanılabilir.
+// bir değişkene atanırsa bu kez expression olur const start = function start(){}; gibi
+// expressionlar tanımlanmadan önce kullanılamaz. Max bunu tavsiye ediyor
+//
+//
+// const start = function() {
+//   // expression
+//   console.log('Game is starting...');
+// };
+
+// const person = {
+//   greet: function greet() {
+//     //şu anda bu method deniyor buna
+//     console.log('Hello there!');
+//   }
+// };
+
+// person.greet();
+
+// console.dir(startGame);
