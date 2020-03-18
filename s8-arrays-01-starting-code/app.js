613d32a23d4bac9b7fe4741b38c1699e74117a96
unknown <ibrahim@unico.aero>
2020-03-18 12:33:55 +0300

    pop, push, unshift, shift

--- s8-arrays-01-starting-code/app.js
+++ s8-arrays-01-starting-code/app.js
@@ -1,33 +1,46 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = new Array(3); // (3)=>3 tane boş array yapar.
-// console.log(moreNumbers);
+// // const moreNumbers = new Array(3); // (3)=>3 tane boş array yapar.
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers); //nothing special
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers); //nothing special
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems); //nodelist
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems); //nodelist
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems); //array
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems); //array
 
-const hobbies = ['Cooking', 'Sport'];
-const personalData = [30, 'Max', { moreDetail: [] }];
+// const hobbies = ['Cooking', 'Sport'];
+// const personalData = [30, 'Max', { moreDetail: [] }];
 
-const analyticsData = [
-  [1, 1.6],
-  [-5.4, 2.1]
-];
+// const analyticsData = [
+//   [1, 1.6],
+//   [-5.4, 2.1]
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoints of data) {
-    console.log(dataPoints);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoints of data) {
+//     console.log(dataPoints);
+//   }
+// }
 
-console.log(personalData[1]);
+// console.log(personalData[1]);
+
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+console.log(poppedValue);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'İbrahim';
+console.log(hobbies);
+console.log(hobbies[3]);
 
 // NOTLAR
 // arraylar zero baseddir yani 0 dan başlar
@@ -36,3 +49,8 @@ console.log(personalData[1]);
 // new Array(3); // (3)=>3 tane boş array yapar.
 // Array.of(1, 2);//nothing special
 // array içine çok değişik türde bilgiler koyabilirsin
+// push sona, unshift başa ekler ve yeni arrayın uzunluğunu return eder
+// pop() sondan 1 kaldırır ve kaldırdığı elemanı string olarak return eder
+// shift() baştan 1 kaldırır
+// shift sağa kaydırır, unshift sola kaydırır gibi düşülmüş isim o yüzden verilmiş
+// pop ve push daha hızlıymış
