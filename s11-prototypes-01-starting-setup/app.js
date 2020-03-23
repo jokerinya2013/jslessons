063fe7b3b85cef782029d9f4d47fd4b405632154
unknown <ibrahim@unico.aero>
2020-03-23 17:45:31 +0300

    prototype, __proto__ ve constructor bakıldı

--- s11-prototypes-01-starting-setup/app.js
+++ s11-prototypes-01-starting-setup/app.js
@@ -1,7 +1,14 @@
-// class Person {
+// class AgedPerson {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// }
+
+// class Person extends AgedPerson {
 //   name = 'Max';
 
 //   constructor() {
+//     super();
 //     this.age = 30;
 //   }
 
@@ -22,9 +29,29 @@ function Person() {
   };
 }
 
-const person = new Person();
-person.greet();
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// };
+
+Person.prototype.printAge = function() {
+  console.log(this.age);
+};
+
+console.dir(Person);
+
+const p = new Person(); // 1. this = {}, 2. add the obj lines to 'this', 3. return this
+p.greet();
+p.printAge();
+console.log(p.__proto__);
+const p2 = new p.__proto__.constructor(); //p ile aynı obji oluşturduk. bire bir aynısı
+console.log(p2);
 
 // Notlar
 //  class yeni bir özellik önceden class yokmuş
 // function İlkharfibüyük () {} olarak tanımlanıyor ve conventional func tanımlaması yapılıyor
+// ilk harfin büyük olması önemli değil önemli olan new ile tanımlama kısmı
+// new şunu yapar 1. this = {}, 2. add properties to empty obj , 3. return this
+// __proto__ tüm objectlerde olur fakat prototype sadece functionlarda olur
+// prototype a yeni fonk eklenebilir
