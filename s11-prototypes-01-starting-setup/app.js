0b8a37879f42ee3b717c8684e727608f1c5bc6e3
unknown <ibrahim@unico.aero>
2020-03-24 16:43:27 +0300

    obj e yeni proprety ekleme, ve özelliklerini düzelme bölümlerine bakıldı. bölüm sonu

--- s11-prototypes-01-starting-setup/app.js
+++ s11-prototypes-01-starting-setup/app.js
@@ -57,14 +57,58 @@ class Person {
 // const p2 = new p.__proto__.constructor(); //p ile aynı obji oluşturduk. bire bir aynısı
 // console.dir(Object.prototype);
 
-const p = new Person();
-console.log(p);
-const p2 = new Person();
-console.log(p.__proto__ === p2.__proto__); //true çünkü aynı memory parçacığını kullanıyorlar
+// const p = new Person();
+// console.log(p);
+// const p2 = new Person();
+// console.log(p.__proto__ === p2.__proto__); //true çünkü aynı memory parçacığını kullanıyorlar
 
-const btn = document.getElementById('btn');
-btn.addEventListener('click', p.greet); // with,  greet = () => {} şeklinde çalışır
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', p.greet); // with,  greet = () => {} şeklinde çalışır
 // btn.addEventListener('click', p.greet.bind(p)); // with greet(){} şeklinde çalışır bu daha efektif ama binlerce clasın varsa
+
+const course = {
+  // new Object
+  title: 'JavaScript - The Complete Guide',
+  rating: 5
+};
+
+// console.log(course.__proto__); ile console.log(Object.getPrototypeOf(course)); aynıdır
+// console.log(Object.getPrototypeOf(course));
+
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course), //olmak zorunda değil
+  printRating: function() {
+    console.log(`${this.rating}/5`); // 5/5
+  }
+});
+//  {} böyle tanımlamanın aynısı
+const student = Object.create(
+  {
+    printProgress: function() {
+      console.log(this.progress);
+    }
+  },
+  {
+    // student.name = 'max'; demek aslında
+    name: {
+      configurable: true,
+      enumerable: true,
+      value: 'Max',
+      writable: true
+    }
+  }
+);
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false
+});
+
+console.log(student);
+student.printProgress(); // 0.8
+course.printRating();
 
 //
 //
