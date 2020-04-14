558a97a2ab8c4b1196963445f6a1da33394e81b0
unknown <ibrahim@unico.aero>
2020-04-14 12:06:58 +0300

    reflect api, proxy api, symbols, iterators, generators gibi advance konulara bakıldı.bölüm sonu

--- s26-metaProgramming/app.js
+++ s26-metaProgramming/app.js
@@ -63,8 +63,66 @@ console.log([...company]);
 // console.log(it.next());
 // console.log(it.next());
 // console.log(it.next());
+// -----------------------------------------------------------------------
+// Reflect API
+const course = {
+  title: 'JS Complete Guide',
+};
 
-//
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title; // course.toString(); yaptığmızda, 'JS Complete Guide' ı verir
+  },
+});
+
+Object.defineProperty(course, 'duration', { value: 52 });
+// bununla aynı işi yapıyor aslında ama meta ölçekli olarak, buradaki tüm
+// Default olarak Property Descriptors true olarak alır. (configurable, enumerable, writable)
+
+Reflect.defineProperty(course, 'year', { value: 2019 });
+// Default olarak Property Descriptors false olarak alır. (configurable, enumerable, writable)
+
+console.log(course);
+// -----------------------------------------------------------------------
+// Proxy API
+
+const courseHandler = {
+  get(obj, propertyName) {
+    console.log(propertyName);
+    if (propertyName === 'length') {
+      return 0;
+    }
+    return obj[propertyName] || 'NOT FOUND'; // property ismi yoksa bu çıktıyı ver
+    // buraya gelen propertyName string olduğu için
+    //  burada obj.propertyName yapamazsın. yaparsan obj."title" yapmaya çalışır hata alırsın
+    // o yüzden obj[propertyName] yaptık bunun çıktısı obj["title"] olur
+  },
+  set(obj, propertyName, newValue) {
+    console.log('Sending data....');
+    if (propertyName === 'rating') {
+      return; // rating i değiştirmek isterse return et, diğerlerini değiştirebilir
+    }
+    obj[propertyName] = newValue;
+  },
+};
+
+const pCourse = new Proxy(course, courseHandler);
+pCourse.rating = 5; // bunu yukarıda şarttan dolayı yapmayacak.
+pCourse.title = 'JaVaScRiPt'; // değişmiş değeri göreceğiz
+console.log(pCourse.title, pCourse.length, pCourse.rating); // JaVaScRiPt 0 NOT FOUND
+
+//============================================================================================
 // Çok kullanmayacağımız özellikler
 // library yazanlar kullanır
 // Symbol() primitive dir ve uniqeness sağlar
+//
+// Reflect API
+// Reflect ana methodu ile çalışır.
+// Object - Reflect farkı, Reflect daha yeni, hataları belirtiyor
+// Reflect de  deletePropery var, Object de yok.
+// delete course.title // kullanmak yerine,
+// Reflect.deleteProperty(course, 'title') // kullan diyor. bu Boolean döner
+//
+// Proxy API
+// normal objleri değiştirmez, bir nevi get, set gibi çalışır.
+//  özelliklere trap denir
